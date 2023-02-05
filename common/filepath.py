4e621813fde028f840387755836b559b8204445f
ike <liuyuqi@honganhome.com>
2023-02-05 18:40:20 +0800

    gui

---
+++ common/filepath.py
@@ -0,0 +1,35 @@
+import os
+
+from pydantic import BaseModel
+
+__all__ = ["FilePath"]
+
+
+class FilePath(BaseModel):
+    """file path，not dir"""
+
+    path: str
+
+    def check_is_valid(self) -> bool:
+        """must file，not dir"""
+        return "." in self.path and not self.path.endswith(".")
+
+    @property
+    def new_path(self) -> "FilePath":
+        """new file path like old，but tag ++"""
+        index = 1
+        while 1:
+            new_path = f"({index:0>2}).".join(self.path.rsplit("."))
+            if os.path.exists(new_path):
+                index += 1
+                continue
+
+            return FilePath(path=new_path)
+
+    def __str__(self):
+        return self.path
+
+    def __format__(self, format_spec):
+        if format_spec == "new":
+            return str(self.new_path)
+        return str(self)
--- main.py
+++ main.py
@@ -10,36 +10,6 @@ SPLIT_TAG_SET = frozenset(SPLIT_TAGS)
 SPLIT_RE = re.compile("|".join(SPLIT_TAGS))
 
 
-class FilePath(BaseModel):
-    """file path，not dir"""
-
-    path: str
-
-    def check_is_valid(self) -> bool:
-        """must file，not dir"""
-        return "." in self.path and not self.path.endswith(".")
-
-    @property
-    def new_path(self) -> "FilePath":
-        """new file path like old，but tag ++"""
-        index = 1
-        while 1:
-            new_path = f"({index:0>2}).".join(self.path.rsplit("."))
-            if os.path.exists(new_path):
-                index += 1
-                continue
-
-            return FilePath(path=new_path)
-
-    def __str__(self):
-        return self.path
-
-    def __format__(self, format_spec):
-        if format_spec == "new":
-            return str(self.new_path)
-        return str(self)
-
-
 class ExcelPath(FilePath):
     """excel path, only xlsx"""
 
@@ -76,6 +46,6 @@ def main(excel_path: str):
     print(f"output to: {new_path}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     file_path = "/Users/ike/Desktop/赠品规则_2023-01-11_12-25-54.xlsx"
     main(file_path)
