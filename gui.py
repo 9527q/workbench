11ed43dea35f983e16c7bde99dc3147fa718c269
ike <liuyuqi@honganhome.com>
2023-02-05 18:32:35 +0800

    gui

---
+++ gui.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import ttk
+
+root_box = tk.Tk()
+
+start_button = tk.Button(root_box, command=lambda : print("hhhh"),
+                         text="开始")
+
+start_button.pack()
+
+
+# 进入消息循环
+root_box.mainloop()
+
